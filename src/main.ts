28a713fff2d32e18190d3d205eb855a726aa6d37
RokuFSD <emilianordx@gmail.com>
2022-07-26 21:12:09 -0300

    Testing welcome screen

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import layout from './styles/layout.module.css';
 import Game from './Game/Game';
 import GameDOM from './Game/GameDOM';
 import GameMediator from './Game/GameMediator';
+import { createElement } from './Helpers';
+
+function welcomeScreen() {
+  let welcome = createElement('div', { class: `${layout.welcome}` }, 'Welcome to Battleship');
+  document.body.appendChild(welcome);
+  setTimeout(() => {
+    welcome.remove();
+  }, 3000);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('mainsvg')!.style.display = 'block';
@@ -13,5 +22,6 @@ app.classList.add(`${layout.app}`);
 
 new GameMediator(Game, GameDOM);
 
+welcomeScreen();
 Game.start();
 Game.makeUI();
